cd9f3d19f0845acec36a6fc54e30600b0a779cfb
Bryan Borgeson <bryborge@gmail.com>
2023-12-26 22:36:17 -0800

    add collections db migration

---
+++ db/migrate/20231227063215_create_collections.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CreateCollections < ActiveRecord::Migration[7.1]
+  def change
+    create_table :collections do |t|
+      t.string :name
+      t.text :description
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
