b141f387d97a26ab36235b87d6056adca5373d4e
Bryan Borgeson <bryborge@gmail.com>
2024-01-18 22:09:18 -0800

    redirect user to their collections page on successful sign in

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -3,16 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe 'User signs in' do
-  it 'with valid credentials' do
-    user = create(:user)
-
-    visit new_user_session_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_button 'Log in'
-
-    expect(page).to have_content('Signed in successfully.')
-  end
+  let(:user) { create(:user) }
 
   it 'with invalid credentials' do
     visit new_user_session_path
@@ -21,5 +12,24 @@ RSpec.describe 'User signs in' do
     click_button 'Log in'
 
     expect(page).to have_content('Invalid Email or password.')
+  end
+
+  describe 'with valid credentials' do
+    before do
+      visit new_user_session_path
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'Log in'
+    end
+
+    it 'succeeds' do
+      expect(page).to have_content('Signed in successfully.')
+    end
+
+    describe 'and is redirected' do
+      it 'to the collections page' do
+        expect(page).to have_current_path(collections_path, ignore_query: true)
+      end
+    end
   end
 end
