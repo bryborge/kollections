2ef3a1ff9ec1d768c8a0a0eed9815af521884565
Bryan Borgeson <bryborge@gmail.com>
2024-01-18 23:34:22 -0800

    don't show 'sign up now' button on home page if user is logged in

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,4 +67,5 @@ RSpec.configure do |config|
 
   # Includes
   config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include Devise::Test::ControllerHelpers, type: :view
 end
--- spec/views/static_pages/home.html.erb_spec.rb
+++ spec/views/static_pages/home.html.erb_spec.rb
@@ -7,4 +7,22 @@ RSpec.describe 'static_pages/home' do
     render
     expect(rendered).to match(/Welcome to Kollections/)
   end
+
+  context 'when user is not signed in' do
+    it 'displays the sign up link' do
+      allow(view).to receive(:user_signed_in?).and_return(false)
+      render
+
+      expect(rendered).to match(/Sign up now/)
+    end
+  end
+
+  context 'when user is signed in' do
+    it 'displays the sign out link' do
+      allow(view).to receive(:user_signed_in?).and_return(true)
+      render
+
+      expect(rendered).not_to have_link('Sign up now')
+    end
+  end
 end
