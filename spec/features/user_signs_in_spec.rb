b8eab86635475491bab8e314cb73518c8196a9a0
Bryan Borgeson <bryborge@gmail.com>
2024-02-18 14:58:37 -0800

    General style clean up for sign up/in flows (#11)

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'User signs in' do
     visit new_user_session_path
     fill_in 'Email', with: 'invalid'
     fill_in 'Password', with: 'invalidpass'
-    click_button 'Log in'
+    click_button 'Sign in'
 
     expect(page).to have_content('Invalid Email or password.')
   end
@@ -19,7 +19,7 @@ RSpec.describe 'User signs in' do
       visit new_user_session_path
       fill_in 'Email', with: user.email
       fill_in 'Password', with: user.password
-      click_button 'Log in'
+      click_button 'Sign in'
     end
 
     it 'succeeds' do
