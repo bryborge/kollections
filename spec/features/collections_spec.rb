c638051f7e86acaf6e245f9f3f4848efd4f200fc
Bryan Borgeson <bryborge@gmail.com>
2024-02-18 14:57:37 -0800

    general style clean up for sign up/in flows

--- spec/features/collections_spec.rb
+++ spec/features/collections_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'Collections' do
     visit new_user_session_path
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
-    click_button 'Log in'
+    click_button 'Sign in'
   end
 
   it 'User views the collections page' do
--- spec/features/user_signs_out_spec.rb
+++ spec/features/user_signs_out_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'User signs out' do
     visit new_user_session_path
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
-    click_button 'Log in'
+    click_button 'Sign in'
 
     # Sign out
     click_button 'Sign Out'
