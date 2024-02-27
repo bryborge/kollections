2dcab51dde5bb2310316b2b6dff6a589a9e979ce
Bryan Borgeson <bryborge@gmail.com>
2024-02-27 14:54:16 +0100

    collections feature test when no collections exist for user

--- spec/features/collections_spec.rb
+++ spec/features/collections_spec.rb
@@ -4,7 +4,6 @@ require 'rails_helper'
 
 RSpec.describe 'Collections' do
   let(:user) { create(:user) }
-  let!(:collections) { create_list(:collection, 3, user:) }
 
   before do
     visit new_user_session_path
@@ -13,23 +12,48 @@ RSpec.describe 'Collections' do
     click_button 'Sign in'
   end
 
-  it 'User views the collections page' do
-    visit collections_path
+  describe 'User views their collections index page' do
+    context 'when User has associated collections' do
+      let!(:collections) { create_list(:collection, 3, user:) }
 
-    # Verify the header is present
-    expect(page).to have_content('Your Collections')
+      before { visit collections_path }
 
-    # Verify each collection is listed with a correct link
-    collections.each do |collection|
-      expect(page).to have_link('View', href: collection_path(collection))
+      it "Displays the user's collections" do
+        # Verify the header is present
+        expect(page).to have_content('Your Collections')
+
+        # Verify each collection is listed with a correct link
+        collections.each do |collection|
+          expect(page).to have_link('View', href: collection_path(collection))
+        end
+
+        # Verify the "Add +" button exists and can be clicked
+        expect(page).to have_link('Add +', href: new_collection_path)
+        click_link 'Add +'
+
+        # Verify that clicking the "Add +" button takes the user to the new collection page
+        expect(page).to have_current_path(new_collection_path)
+      end
     end
 
-    # Verify the "Add +" button exists and can be clicked
-    expect(page).to have_link('Add +', href: new_collection_path)
-    click_link 'Add +'
+    context 'when User does not have associated collections' do
+      before { visit collections_path }
 
-    # Verify that clicking the "Add +" button takes the user to the new collection page
-    # Update the path as per your routes
-    expect(page).to have_current_path(new_collection_path)
+      it "Displays a 'no collections' message" do
+        # Verify the header is present
+        expect(page).to have_content('Your Collections')
+
+        # Verify no collections message is present and no collections are displayed
+        expect(page).to have_content("You don't have any collections.")
+        expect(page).not_to have_css('.card h-100')
+
+        # Verify the "Add +" button exists and can be clicked
+        expect(page).to have_link('Add +', href: new_collection_path)
+        click_link 'Add +'
+
+        # Verify that clicking the "Add +" button takes the user to the new collection page
+        expect(page).to have_current_path(new_collection_path)
+      end
+    end
   end
 end
