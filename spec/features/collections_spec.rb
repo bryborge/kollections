2e4651a92a4fa97117df16e5feb0d8b9c6fc47cd
Bryan Borgeson <bryborge@gmail.com>
2024-02-27 23:05:49 +0100

    feature test for number of items in collection displays correctly

--- spec/features/collections_spec.rb
+++ spec/features/collections_spec.rb
@@ -24,26 +24,44 @@ RSpec.describe 'Collections' do
       expect(page).to have_current_path(new_collection_path)
     end
 
-    context 'when User has associated collections' do
-      let!(:collections) { create_list(:collection, 3, user:) }
-
-      before { visit collections_path }
-
-      it "Displays the user's collections" do
-        # Verify each collection is listed with a correct link
-        collections.each do |collection|
-          expect(page).to have_link('View', href: collection_path(collection))
-        end
-      end
-    end
-
     context 'when User does not have associated collections' do
       before { visit collections_path }
 
       it 'does not display any collections' do
         # Verify no collections message is present and no collections are displayed
         expect(page).to have_content("You don't have any collections.")
-        expect(page).not_to have_css('.card h-100')
+        expect(page).not_to have_css('.card.h-100')
+      end
+    end
+
+    context 'when User has associated collections' do
+      let!(:collections) { create_list(:collection, 3, user:) }
+
+      before do
+        # generate a random number of items per collection
+        collections.each do |collection|
+          create_list(:item, rand(0..3), collection:)
+        end
+
+        visit collections_path
+      end
+
+      it "displays the user's collections" do
+        expect(page).to have_css('.card.h-100')
+
+        # Verify each collection is listed with a correct link
+        collections.each do |collection|
+          expect(page).to have_link('View', href: collection_path(collection))
+        end
+      end
+
+      it 'displays the number of items for each collection' do
+        collections.each do |collection|
+          expected_item_count = collection.items.count
+          item_count_element  = find(".collection[data-collection-id='#{collection.id}']")
+
+          expect(item_count_element.text).to eq(expected_item_count.to_s)
+        end
       end
     end
   end
