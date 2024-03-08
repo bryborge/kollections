d5f3929acc1634bae70d11e71f9005f67e75f7e3
Bryan Borgeson <bryborge@gmail.com>
2024-03-07 22:05:05 -0800

    seed the initial collections I want to track items under

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -8,3 +9,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+user = User.find_by(email: 'bryborge@gmail.com')
+
+Collection.find_or_create_by!(name: 'Comic Book', kind: 'comics', user_id: user.id)
+Collection.find_or_create_by!(name: 'Movie', kind: 'movies', user_id: user.id)
+Collection.find_or_create_by!(name: 'Music', kind: 'music', user_id: user.id)
