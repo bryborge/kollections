246d60c845d0070890d60ef9eeceffbe74279223
Bryan Borgeson <bryborge@gmail.com>
2024-03-25 21:24:22 -0700

    fix js interactions (#18)
    * fix mobile view toggler; load bootstrap js
    * fix confirmation dialog box on delete collection/item button press

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,18 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import * as bootstrap from "bootstrap"
+import "popper"
+import "bootstrap"
+
+// Delete [Collection | Item] confirmation dialog box
+//   e.g. app/views/collections/edit.html.erb
+document.addEventListener("turbo:click", function(event) {
+  const element = event.target
+  const confirmMessage = element.getAttribute("data-turbo-confirm")
+  if (!confirmMessage || window.confirm(confirmMessage)) {
+    return true
+  }
+
+  event.preventDefault()
+  return false
+})
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,4 +6,6 @@ pin 'application', preload: true
 pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
+pin 'popper', to: 'https://unpkg.com/@popperjs/core@2/dist/umd/popper.js'
+pin 'bootstrap', to: 'https://unpkg.com/bootstrap@5/dist/js/bootstrap.min.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
